a55277c7f73fb4d160993b8a8270cdc08b40d0e8
Nicolas Lang <git@nicolas-lang.de>
2021-06-20 10:35:17 +0200

    new images for entity and icons

--- data.lua
+++ data.lua
@@ -1,1 +1,6 @@
-log("data")
+--LOAD PROTOTYPES
+require("graphics.imageInfo")
+require("prototypes.warehouse_subgroup")
+require("prototypes.warehouse_entity")
+require("prototypes.warehouse_signal_pole")
+require("prototypes.warehouse_tech")
